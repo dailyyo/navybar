28f291d1c197f22c5694dbd9e1bb89378aebaaaa
dailyyo <prime.nvzh@outlook.com>
2018-07-06 09:26:43 +0700

    Edit header dashboard

--- application/views/admin/dashboard/dashboard.php
+++ application/views/admin/dashboard/dashboard.php
@@ -2,7 +2,7 @@
         <div class="col-sm-4">
             <div class="page-header float-left">
                 <div class="page-title">
-                    <h1>Administrator Dashboard</h1>
+                    <h1>Dashboard Administrator</h1>
                 </div>
             </div>
         </div>
